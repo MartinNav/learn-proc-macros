d9419d3cd9136451dfb73d7a44b69ad4e9dcd2f6
MartinNav <martinnavratilu@gmail.com>
2026-04-29 21:41:34 +0200

    only first is passing

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,24 @@
+use core::convert::From;
+
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    //eprintln!("TOKENS: {}", input);
+    eprintln!("AST: {:#?}",ast);
+    let expanded = quote!{
+        
+        #[automatically_derived]
+        impl #name {
+            pub fn builder()-> #name {
 
-    unimplemented!()
+            }
+        }
+
+    };
+    TokenStream::from(expanded)
 }

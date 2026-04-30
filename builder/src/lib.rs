7e280517b0c55f9fe81f5b88e652528658d7d32a
MartinNav <martinnavratilu@gmail.com>
2026-04-30 18:10:58 +0200

    The third test passed

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -9,7 +9,19 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
     let name = &ast.ident;
     //eprintln!("TOKENS: {}", input);
-    eprintln!("AST: {:#?}",ast);
+    //eprintln!("AST: {:#?}",ast);
+    let data = match &ast.data {
+        syn::Data::Struct(data) => data,
+        _ => panic!("Builder can only be derived for structs"),
+    };
+    let fields = match &data.fields {
+        syn::Fields::Named(fields) => &fields.named,
+        _ => panic!("Builder can only be derived for structs with named fields"),
+    };
+    let field_names:Vec<syn::Ident> = fields.iter().map(|f| f.ident.clone()).filter(|x| x.is_some()).map(|x| x.unwrap()).collect::<Vec<_>>();
+    let field_types :Vec<syn::Type> = fields.iter().map(|f| f.ty.clone()).collect::<Vec<_>>();
+
+
     let bident = syn::Ident::new(&format!("{}Builder", name), name.span());
     let expanded = quote!{
         pub struct #bident {
@@ -28,6 +40,15 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     current_dir: None,
                 }
             }
+        }
+        impl #bident {
+            #(
+                pub fn #field_names(&mut self, #field_names: #field_types) -> &mut Self {
+                    self.#field_names = Some(#field_names);
+                    self
+                }
+
+            )*
         }
 
     };
